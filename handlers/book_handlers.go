a900f251026e7b9e804d790bb31bebf471f8bda0
Sevi <sevtaphazim@gmail.com>
2025-07-15 22:41:43 +0100

    added gorm tags to database

--- handlers/book_handlers.go
+++ handlers/book_handlers.go
@@ -1,10 +1,48 @@
 package handlers
 
-import "library/services"
+import (
+	_"library/services"
+	_"net/http"
+)
 
-type BookHandler struct {
-	Service *services.BookService
-}
+// type BookHandler struct {
+// 	Service *services.BookService
+// }
+
+// func createBook(w http.ResponseWriter, r *http.Request) 
+// var req *models.library
+// 	err = json.NewDecoder(r.Body).Decode(&req)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
+// 	userID := r.URL.Query().Get("bookID")  
+// 	if req.Title == "" || req.Text == "" {
+// 		http.Error(w, "title, text required", http.StatusBadRequest)
+// 		return
+// 	}
+	
+// 	req.UserID = userID
+// 	req.ID = uuid.NewString()
+// 	err = database.Db.Create(&req).Error
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	json.NewEncoder(w).Encode(req)
+
+// func updateBook(w http.ResponseWriter, r *http.Request) 
+
+
+// func deleteBook(w http.ResponseWriter, r *http.Request) 
+
+
+
+
+
+
 
 /*
 create book - admin
