c5c7aa35ea26b9ab9530793499e36db47e5b8b36
JJprog25 <jordanjdlwilliams@gmail.com>
2025-07-16 01:57:37 +0100

    First commit, Year and Title handlers added

--- handlers/book_handlers.go
+++ handlers/book_handlers.go
@@ -6,6 +6,64 @@ type BookHandler struct {
 	Service *services.BookService
 }
 
+func (h *BookHandler) FindByGenre(w http.ResponseWriter, r *http.Request){
+genre := r.URL.Query(.Get("genre")
+
+books, err := h.service.FindByGenre(genre)
+if err != nil{
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return 
+}
+json.NewEncoder(w).Encode(books)
+}
+
+func (h *BookHandler) FindByYear(w http.ResponseWriter, r *http.Request) {
+	year := r.URL.Query().Get("year")
+
+	books, err := h.service.FindByYear(year)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+	json.NewEncoder(w).Encode(books)
+        return
+    }
+
+	json.NewEncoder(w).Encode(books)
+
+func (h *BookHandler) FindByTitle(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Query().Get("title")
+
+	books, err := h.service.FindByTitle(title)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+	json.NewEncoder(w).Encode(books)
+}
+
+
+
+
+
+/*
+func (h *BookHandler) FindByTitle(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Query().Get("title")
+
+	var results []Book
+	for _, book := range books {
+		if title != "" && book.Title == title {
+			results = append(results, book)
+		}
+	}
+
+	json.NewEncoder(w).Encode(results)
+}
+
+*/
+
 /*
 Group 1
 list All Books - user
